650b9e3e495c4e1974cb0b6b640724a297b67fb1
Marcosegm <marcos-gm14@hotmail.com>
2023-07-06 17:43:04 -0500

    update files

--- main.py
+++ main.py
@@ -1,47 +1,30 @@
-from fastapi import FastAPI, Body, Path, Query, Request, HTTPException, Depends
+from fastapi import  FastAPI
 from fastapi.responses import HTMLResponse, JSONResponse
-from pydantic import BaseModel, Field
-from typing import Optional, List
-from jwt_manager import create_token, validate_token
+from pydantic import BaseModel
+from utils.jwt_manager import create_token
 from fastapi.security import HTTPBearer
-from models.movie import Movie as MovieModel
-from config.database import Session, engine, Base
+from config.database import engine, Base
+from middlewares.error_handler import ErrorHandler
+from routers.movie import movie_router
+from routers.user import user_router
 
 app = FastAPI()
 app.title = "Mi aplicacion con FastAPI"
 app.version = "0.0.1"
 
-class JWTBearer(HTTPBearer):
-    async def __call__(self, request: Request):
-        auth = await super().__call__(request)
-        data = validate_token(auth.credenciales)
-        if data["email"] != "admin":
-            raise HTTPException(status_code=403, detail="Credenciales son invalidas")
+app.add_middleware(ErrorHandler)
+
+app.include_router(movie_router)
+app.include_router(user_router)
+
 
 
 class User(BaseModel):
     email: str
     password: str
 
-class Movie(BaseModel):
-    id: Optional[int] = None
-    title: str = Field( min_length= 5, max_length=15)
-    overview: str = Field( min_length= 15, max_length=50)
-    year: int = Field(le=2022)
-    rating: float = Field(ge=1, le=10)
-    category: str = Field(min_length=5, max_length=15)
 
-    class Config:
-        schema_extra = {
-            "example": {
-                'id': 1,
-                'title': 'Mi pelicula',
-                'overview': "Descripcion de la pelicula",
-                'year': 2022,
-                'rating': 9.8,
-                'category': 'Acción'  
-            }
-        }
+   
 
 # gt: greater than
 # ge: greater than or equal
@@ -78,47 +61,4 @@ def login(user: User):
         return JSONResponse(status_code=200 ,content=token)
     else:
         return JSONResponse(status_code=401, content={"message": "Credenciales inválidas, intente de nuevo"})
-
-@app.get("/movies", tags=["movies"], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
-def get_movies() -> List[Movie]:
-    return JSONResponse(content=movies, status_code=200)
-
-@app.get("/movies/{id}",tags=["movies"], response_model=Movie)
-def get_movie(id: int = Path(ge= 1, le=2000)) -> Movie:
-    for item in movies:
-        if item["id"] == id:
-            return JSONResponse(content=item)
-    return JSONResponse(status_code=404,content=[])
-
-
-@app.get("/movies/", tags=["movies"], response_model=List[Movie])
-def get_movies_by_category(category: str = Query(min_length=5, max_length=15)) -> List[Movie]:
-    data = [item for item in movies if item["category"] == category]
-    return JSONResponse(content=data)
-
-@app.post('/movies', tags=['movies'], response_model=dict, status_code=201)
-def create_movie(movie: Movie) -> dict:
-    db = Session()
-    new_movie = MovieModel(**movie.dict())
-    db.add(new_movie)
-    db.commit()
-    return JSONResponse(status_code=201, content={"message": "Se ha registrado la película"})
-
-@app.put("/movies/{id}", tags=["movies"], response_model=dict, status_code=200)
-def update_movie(id: int , movie: Movie ) -> dict:
-    for item in movies:
-        if item["id"] == id:
-            item["title"] = movie.title
-            item["overview"] = movie.overview
-            item["year"] = movie.year
-            item["rating"] = movie.rating
-            item["category"] = movie.category
-            return JSONResponse(content={"message": "Se ha modificado la películla"}, status_code=200)
-
-@app.delete("/movies/{id}", tags=["movies"], response_model=dict, status_code=200)
-def delete_movie(id:int) -> dict:
-    for item in movies:
-        if item["id"] == id:
-            movies.remove(item)
-            return JSONResponse(content={"message": "Se ha eliminado la películla"}, status_code=200)
 
--- utils/jwt_manager.py
+++ utils/jwt_manager.py
@@ -1,7 +1,7 @@
 from jwt import encode, decode
 
 def create_token(data: dict) -> str:
-    token: str = encode(payload=data, key="my_secrete_key", algorithm="HS256")
+    token: str = encode(payload=data, key="my_secret_key", algorithm="HS256")
     return token
 
 def validate_token(token: str) -> dict:
